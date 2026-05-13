0aaf75a96586e4641386c27b533146e9daf4d084
laila <laila@bomba.gov.my>
2026-05-13 12:43:58 +0800

    create about page

---
+++ app/routes/about.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our website! We are dedicated to providing the best service possible.
+      </p>
+      <p>
+        Feel free to explore and learn more about what we do.
+      </p>
+    </div>
+  );
+};
+
+export default About;
